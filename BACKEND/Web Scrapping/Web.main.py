58bf21a41f276ac0ab4254a3c12beccde20987a3
ADlv19 <adwaitg02@gmail.com>
2021-12-15 21:15:04 +0530

    adwait

--- BACKEND/Web Scrapping/Web.main.py
+++ BACKEND/Web Scrapping/Web.main.py
@@ -1,15 +1,13 @@
 from selenium import webdriver
+from linkedin_scraper import Person,actions
 import time
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
+
 PATH = 'D:\\Py New\\Py Softwares\\Selenium\\chromedriver.exe'
 driver = webdriver.Chrome(PATH)
 
-for i in range(5):
-    driver.get("https://www.linkedin.com/in/aju-palleri-248798a4/")
-    driver.implicitly_wait(10)
-    time.sleep(5)
-    # driver.find_element(By.XPATH, '//button[@class="authwall-join-form__form-toggle--bottom form-toggle"]').click()
-    # driver.maximize_window()
-    # driver.find_element(By.XPATH, '//input[@id="username"]').send_keys
+email = 'idontknowimjustabirmd@gmail.com'
+password = '12345Idontknow'
+
