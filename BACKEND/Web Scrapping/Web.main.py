3b95c07a3807ac86367cf6019f23aa57fe650200
ADlv19 <adwaitg02@gmail.com>
2021-12-15 20:32:51 +0530

    adwait

--- BACKEND/Web Scrapping/Web.main.py
+++ BACKEND/Web Scrapping/Web.main.py
@@ -5,8 +5,11 @@ from selenium.webdriver.common.by import By
 
 PATH = 'D:\\Py New\\Py Softwares\\Selenium\\chromedriver.exe'
 driver = webdriver.Chrome(PATH)
-driver.get("https://www.linkedin.com")
-driver.implicitly_wait(10)
-#driver.find_element(By.XPATH, '//button[@class="authwall-join-form__form-toggle--bottom form-toggle"]').click()
-#driver.maximize_window()
-time.sleep(3)
+
+for i in range(5):
+    driver.get("https://www.linkedin.com/in/aju-palleri-248798a4/")
+    driver.implicitly_wait(10)
+    time.sleep(5)
+    # driver.find_element(By.XPATH, '//button[@class="authwall-join-form__form-toggle--bottom form-toggle"]').click()
+    # driver.maximize_window()
+    # driver.find_element(By.XPATH, '//input[@id="username"]').send_keys
