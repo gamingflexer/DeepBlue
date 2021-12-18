b4dda0821da98bad79f7d67f8f71cde2f6488c22
ADlv19 <adwaitg02@gmail.com>
2021-12-18 20:17:50 +0530

    adwait

--- BACKEND/Web Scrapping/Web.main.py
+++ BACKEND/Web Scrapping/Web.main.py
@@ -1,6 +1,8 @@
 import time
+import
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 PATH = 'D:\\Py New\\Py Softwares\\Selenium\\chromedriver.exe'
 driver = webdriver.Chrome(PATH)
@@ -9,4 +11,7 @@ email = 'idontknowimjustabirmd@gmail.com'
 password = '12345Idontknow'
 
 driver.get('https://www.linkedin.com/login')
+driver.maximize_window()
+usernameField = driver.find_element(By.ID, 'username')
+passwordField = driver.find_element(By.ID, 'password')
 time.sleep(10)
