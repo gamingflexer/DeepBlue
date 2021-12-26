a85dcc377a67457e2b8a0ed7d9390ecf95676b72
ADlv19 <adwaitg02@gmail.com>
2021-12-26 15:39:52 +0530

    AD.wait

--- BACKEND/Web Scrapping/GitHub_Scrap/githubFunctions.py
+++ BACKEND/Web Scrapping/GitHub_Scrap/githubFunctions.py
@@ -1,12 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-#PATH = 'D:\\Py New\\Py Softwares\\Selenium\\chromedriver.exe'
-#driver = webdriver.Chrome(PATH)
 
-
-def getContributions(driver):
+def getContributions(userURL, driver):
+    driver.get(userURL)
     text = driver.find_element(By.XPATH, '//h2[@class="f4 text-normal mb-2"]')
     print(text.text)
+    driver.quit()
     return text.text
+
+
+def getUserRepos(repoURL, driver):
+    driver.get(repoURL)
+    repos = driver.find_element(By.ID, 'user-repositories-list')
+    print(repos.text)
+    driver.quit()
+    return repos.text
+
 
--- BACKEND/Web Scrapping/GitHub_Scrap/github_scrap_main.py
+++ BACKEND/Web Scrapping/GitHub_Scrap/github_scrap_main.py
@@ -3,12 +3,9 @@ import githubFunctions as gf
 from selenium.webdriver.common.by import By
 import time
 import os
-
 PATH = 'D:\\Py New\\Py Softwares\\Selenium\\chromedriver.exe'
 driver = webdriver.Chrome(PATH)
 
-driver.get('https://github.com/gamingflexer')
+userURL = 'https://github.com/gamingflexer'
+reposURL = userURL + '?tab=repositories'
 
-gf.getContributions(driver)
-
-driver.quit()
