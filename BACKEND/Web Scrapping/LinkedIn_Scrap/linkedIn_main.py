ae2d24783200ee25e7e0c4dc5061e500c9abe875
ADlv19 <adwaitg02@gmail.com>
2021-12-27 20:43:17 +0530

    AD.wait

---
+++ BACKEND/Web Scrapping/LinkedIn_Scrap/linkedIn_main.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+PATH = 'D:\\Py New\\Py Softwares\\Selenium\\chromedriver.exe'
+
+# open linkedin.com
+driver = webdriver.Chrome(PATH)
+driver.get("https://www.linkedin.com/login")
+driver.implicitly_wait(2)
+
+# get username , password tag
+driver.find_element(By.ID, "username").send_keys("adwaitg02@gmail.com")
+driver.find_element(By.ID, "password").send_keys("aA15042002!")
+
+# Cilck on sign in button
+driver.find_element(By.XPATH, "//button[@type='submit']").click()
+
+linkOfTab = 'www.github.com'
+# Open New Tab
+driver.execute_script("window.open('"+linkOfTab+"')")
+
+""" 
+    To Switch Tabs by Index Number
+    driver.switch_to.window(driver.window_handles[0])
+"""
