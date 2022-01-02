df668c37c97ec083bea14a48567a6918fd267c17
ADlv19 <adwaitg02@gmail.com>
2022-01-02 21:02:46 +0530

    AD.wait

--- BACKEND/Web Scrapping/LinkedIn_Scrap/linkedIn_main.py
+++ BACKEND/Web Scrapping/LinkedIn_Scrap/linkedIn_main.py
@@ -5,21 +5,28 @@ from selenium.webdriver.common.by import By
 
 PATH = 'D:\\Py New\\Py Softwares\\Selenium\\chromedriver.exe'
 
+# USERNAME AND PASSWORD
+
+USERNAME = ''
+PASSWORD = ''
+
 # open linkedin.com
 driver = webdriver.Chrome(PATH)
 driver.get("https://www.linkedin.com/login")
 driver.implicitly_wait(2)
 
 # get username , password tag
-driver.find_element(By.ID, "username").send_keys("")
-driver.find_element(By.ID, "password").send_keys("")
+driver.find_element(By.ID, "username").send_keys(USERNAME)
+driver.find_element(By.ID, "password").send_keys(PASSWORD)
 
 # Cilck on sign in button
 driver.find_element(By.XPATH, "//button[@type='submit']").click()
 
 linkOfTab = 'www.github.com'
 # Open New Tab
-driver.execute_script("window.open('"+linkOfTab+"')")
+#driver.execute_script("window.open('')")
+
+driver.find_element(By.CLASS_NAME, 'search-global-typeahead__input always-show-placeholder').send_keys(Keys.CONTROL + 't')
 
 """ 
     To Switch Tabs by Index Number
