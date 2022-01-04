b7c2609ca72b12fffb53e541ec1b215218e09ca9
ADlv19 <adwaitg02@gmail.com>
2022-01-04 13:47:31 +0530

    AD.wait

--- BACKEND/Web Scrapping/Web.main.py
+++ BACKEND/Web Scrapping/Web.main.py
@@ -1,5 +1,6 @@
 import time
 from selenium import webdriver
+from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 
 PATH = 'D:\\Py New\\Py Softwares\\Selenium\\chromedriver.exe'
@@ -11,7 +12,8 @@ driver.get('https://www.linkedin.com/login')
 driver.maximize_window()
 driver.implicitly_wait(4)
 
-driver.find_element(By.ID, 'username').send_keys(email)
+driver.find_element_by_id('username').send_keys(email)
+driver.find_element(By.ID, 'username').send_keys(Keys.CONTROL + 't')
 driver.find_element(By.ID, 'password').send_keys(password)
 time.sleep(2)
 driver.find_element(By.CLASS_NAME, 'login__form_action_container ').click()
