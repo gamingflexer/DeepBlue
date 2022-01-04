814aa56a022f92cefd7e7135a291dd0082f593db
ADlv19 <adwaitg02@gmail.com>
2022-01-04 11:37:15 +0530

    AD.wait

--- BACKEND/Web Scrapping/GitHub_Scrap/github_scrap_main.py
+++ BACKEND/Web Scrapping/GitHub_Scrap/github_scrap_main.py
@@ -2,7 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 PATH = 'D:\\Py New\\Py Softwares\\Selenium\\chromedriver.exe'
 driver = webdriver.Chrome(PATH)
+from githubFunctions import *
 
 userURL = 'https://github.com/gamingflexer'
 reposURL = userURL + '?tab=repositories'
 
+print(getContributions(driver, userURL))
