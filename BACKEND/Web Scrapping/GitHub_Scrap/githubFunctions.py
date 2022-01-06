41e018703e5c41b6cdd58035f245a135d97f6fb2
ADlv19 <adwaitg02@gmail.com>
2022-01-06 15:03:02 +0530

    Completed Extracting Repo Name and Stars

--- BACKEND/Web Scrapping/GitHub_Scrap/githubFunctions.py
+++ BACKEND/Web Scrapping/GitHub_Scrap/githubFunctions.py
@@ -1,18 +1,18 @@
 from selenium.webdriver.common.by import By
 
-def getContributions(userURL, driver):
-    driver.get(userURL)
-    text = driver.find_element(By.XPATH, '//h2[@class="f4 text-normal mb-2"]')
-    print(text.text)
-    driver.quit()
-    return text.text
 
+class Github:
+    def getContributions(self, userURL, driver):
+        driver.get(userURL)
+        text = driver.find_element(By.XPATH, '//h2[@class="f4 text-normal mb-2"]')
+        print(text.text)
+        return text.text
 
-def getUserRepos(repoURL, driver):
-    driver.get(repoURL)
-    repos = driver.find_element(By.ID, 'user-repositories-list')
-    print(repos.text)
-    driver.quit()
-    return repos.text
-
-
+    def getUserRepos(self, repoURL, driver):
+        driver.get(repoURL)
+        repos = driver.find_element(By.ID, 'user-repositories-list')
+        #print(repos.text)
+        #driver.quit()
+        value = driver.find_element_by_xpath('/html/body/div[4]/main/div[2]/div/div[2]/div[2]/div/div[2]/ul/li[1]/div[1]/div[3]/a/text()')
+        print(value)
+        return repos.text
--- BACKEND/Web Scrapping/GitHub_Scrap/github_scrap_main.py
+++ BACKEND/Web Scrapping/GitHub_Scrap/github_scrap_main.py
@@ -1,10 +1,38 @@
+import time
+
+from _cffi_backend import string
+from bs4 import BeautifulSoup
+import requests
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+
 PATH = 'D:\\Py New\\Py Softwares\\Selenium\\chromedriver.exe'
 driver = webdriver.Chrome(PATH)
 from githubFunctions import *
 
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
 userURL = 'https://github.com/gamingflexer'
 reposURL = userURL + '?tab=repositories'
 
-print(getContributions(driver, userURL))
+html_content = requests.get(reposURL).text
+soup = BeautifulSoup(html_content, 'html.parser')
+star_count = soup.find_all('a', attrs={'class': 'Link--muted mr-3'})
+repo_name = soup.find_all('p', attrs={'class': 'col-9 d-inline-block color-fg-muted mb-2 pr-4'})
+
+i = []
+j = []
+
+for tag in star_count:
+    i.append(tag.text.strip())
+
+for tag in repo_name:
+    j.append(tag.text.strip())
+
+'''for k in range(len(j)):
+    j[k] = j[k].split('\n')
+    j[k] = [j[k] for j[k] in j[k] if j[k].strip()]
+'''
+
+for index in range(len(j)):
+    print(j[index], i[index])
