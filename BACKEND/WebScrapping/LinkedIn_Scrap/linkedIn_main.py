e526d2bce6c7ba2b089e3d9980317e4b64149d70
ADlv19 <adwaitg02@gmail.com>
2022-02-01 21:01:27 +0530

    adwait

--- BACKEND/WebScrapping/LinkedIn_Scrap/linkedIn_main.py
+++ BACKEND/WebScrapping/LinkedIn_Scrap/linkedIn_main.py
@@ -1,36 +1,69 @@
 from selenium import webdriver
 from selenium.webdriver import ActionChains
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+import requests
+import re
+import xml.etree.ElementTree as ET
 
-PATH = 'D:\\Py New\\Py Softwares\\Selenium\\chromedriver.exe'
+# PATH to chrome driver
+PATH = 'D:\\Softwares\\chromedriver.exe'
+ser = Service(PATH)
+op = webdriver.ChromeOptions()
+driver = webdriver.Chrome(service=ser, options=op)
 
 # USERNAME AND PASSWORD
-
-USERNAME = ''
+USERNAME = 'adwaitg02@gmail.com'
 PASSWORD = ''
 
 # open linkedin.com
-driver = webdriver.Chrome(PATH)
 driver.get("https://www.linkedin.com/login")
 driver.implicitly_wait(2)
 
-# get username , password tag
+# get Username , Password tag and send details and Hit Enter
 driver.find_element(By.ID, "username").send_keys(USERNAME)
 driver.find_element(By.ID, "password").send_keys(PASSWORD)
-
-# Cilck on sign in button
 driver.find_element(By.XPATH, "//button[@type='submit']").click()
 
-linkOfTab = 'https://www.github.com/'
-# Open New Tab
-driver.execute_script("window.open('"+linkOfTab+"','_blank');")
+# GOTO REQUIRED PERSON PROFILE
+driver.get('https://www.linkedin.com/in/aju-palleri-248798a4/')
 
-driver.find_element(By.CLASS_NAME, 'search-global-typeahead__input always-show-placeholder').send_keys(Keys.CONTROL + 't')
+pagesource = driver.page_source
+soup = BeautifulSoup(pagesource, "html.parser")
+aboutWithTags = soup.find('div', attrs={'class', 'inline-show-more-text inline-show-more-text--is-collapsed'})
+
+"""def remove_html_markup(s):
+    tag = False
+    quote = False
+    out = ""
+
+    for c in s:
+            if c == '<' and not quote:
+                tag = True
+            elif c == '>' and not quote:
+                tag = False
+            elif (c == '"' or c == "'") and tag:
+                quote = not quote
+            elif not tag:
+                out = out + c
+
+    return out
+"""
+
+
+def remove_html_tags(text):
+    """Remove html tags from a string"""
+    import re
+    clean = re.compile('<.*?>')
+    return re.sub(clean, '', text)
+
+
+tagsWithout = remove_html_tags(aboutWithTags)
+print(tagsWithout)
 
 """
     To Switch Tabs by Index Number
     driver.switch_to.window(driver.window_handles[0])
 """
-
-
--- BACKEND/WebScrapping/Web.main.py
+++ BACKEND/WebScrapping/Web.main.py
@@ -1,22 +1,28 @@
-import time
+from linkedin_scraper import Person, actions
 from selenium import webdriver
-from selenium.webdriver import Keys
-from selenium.webdriver.common.by import By
+driver = webdriver.Chrome(executable_path=r"D:\\Softwares\\chromedriver.exe")
+email = "adwaitg02@gmail.com"
+password = ""
+actions.login(driver, email, password) # if email and password isnt given, it'll prompt in terminal
 
-PATH = 'D:\\Py New\\Py Softwares\\Selenium\\chromedriver.exe'
-driver = webdriver.Chrome(PATH)
+person = Person("https://www.linkedin.com/in/aju-palleri-248798a4/", driver=driver)
+print("Person: " + person.name)
+print(person.about)
+print(person.accomplishments)
+print(person.educations)
+print(person.experiences)
 
-email = 'idontknowimjustabirmd@gmail.com'
-password = '12345Idontknow'
-driver.get('https://www.linkedin.com/login')
-driver.maximize_window()
-driver.implicitly_wait(4)
 
-driver.find_element_by_id('username').send_keys(email)
-driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + 't')
-driver.find_element(By.ID, 'password').send_keys(password)
-time.sleep(2)
-driver.find_element(By.CLASS_NAME, 'login__form_action_container ').click()
 
-time.sleep(10)
-driver.quit()
+
+
+
+
+
+
+
+
+
+
+
+
