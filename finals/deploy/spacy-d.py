e95942790df4b5a6eeaafa332ddc9b3c78f60298
gamingflexer <osurve20ece@student.mes.ac.in>
2022-02-12 12:36:28 +0530

    d

--- finals/deploy/spacy-d.py
+++ finals/deploy/spacy-d.py
@@ -1,18 +1,12 @@
 import spacy
 from tika import parser
 
-#prediction - 1
-fname = '' #add a recuuring file function
-raw = parser.from_file(fname)
-text = raw['content']
-
-#model paths
+# model paths
 model_spacy_path_all = '700 old code'
 
 model_spacy = spacy.load(model_spacy_path_all)
 
-#predict
-doc = model_spacy(raw['content'])
+# predict
+doc = model_spacy('first data')
 for ent in doc.ents:
     print(f'{ent.label_.upper():{30}} - {ent.text}')
-    
