3f44eb69f93ae1bbca34cbdcc6428030b8548c7b
gamingflexer <osurve20ece@student.mes.ac.in>
2022-02-22 19:52:55 +0530

    new

--- flask/zero-shot-campare.py
+++ flask/zero-shot-campare.py
@@ -1,19 +1,20 @@
 # !pip install transformers==3.1.0
 
 from transformers import pipeline
-classifier = pipeline("zero-shot-classification")  # try a smaller model
+# try a smaller model
 
 text1 = " "  # text to classify
 
 
-def compare1(text):
+def comparemain(text):
+
+    classifier = pipeline("zero-shot-classification")
     # classes to divide into or not
     candidate_labels = ["skills", "education", "experience"]
     output = classifier(text, candidate_labels, multi_class=False)
-    return output
 
-
-compare1(text1)
-final_list = {output['sequence']: output['labels'][2]}
+    # last element of the dict
+    final_list = {output['sequence']: output['labels'][2]}
+    return final_list
 
 # take the output of final output as a comparsions
