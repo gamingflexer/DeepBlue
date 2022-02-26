5b6c466a3a461a18b0a588d1d91446ffdc2215af
gamingflexer <osurve20ece@student.mes.ac.in>
2022-02-27 01:35:55 +0530

    n5

--- flask/binary.py
+++ flask/binary.py
@@ -159,62 +159,6 @@ def upload():
 
                         # model eval
 
-                        def process_resume2(text, tokenizer, max_len):
-                            tok = tokenizer.encode_plus(
-                                text, max_length=max_len, return_offsets_mapping=True)
-
-                            curr_sent = dict()
-
-                            padding_length = max_len - len(tok['input_ids'])
-
-                            curr_sent['input_ids'] = tok['input_ids'] + \
-                                ([0] * padding_length)
-                            curr_sent['token_type_ids'] = tok['token_type_ids'] + \
-                                ([0] * padding_length)
-                            curr_sent['attention_mask'] = tok['attention_mask'] + \
-                                ([0] * padding_length)
-
-                            final_data = {
-                                'input_ids': torch.tensor(curr_sent['input_ids'], dtype=torch.long),
-                                'token_type_ids': torch.tensor(curr_sent['token_type_ids'], dtype=torch.long),
-                                'attention_mask': torch.tensor(curr_sent['attention_mask'], dtype=torch.long),
-                                'offset_mapping': tok['offset_mapping']
-                            }
-
-                        def predict(model, tokenizer, idx2tag, tag2idx, device, text):
-                            model.eval()
-                            data = process_resume2(text, tokenizer, MAX_LEN)
-                            input_ids, input_mask = data['input_ids'].unsqueeze(
-                                0), data['attention_mask'].unsqueeze(0)
-                            labels = torch.tensor([1] * input_ids.size(0),
-                                                  dtype=torch.long).unsqueeze(0)
-                            with torch.no_grad():
-                                outputs = model(
-                                    input_ids,
-                                    token_type_ids=None,
-                                    attention_mask=input_mask,
-                                    labels=labels,
-                                )
-                                tmp_eval_loss, logits = outputs[:2]
-
-                            logits = logits.cpu().detach().numpy()
-                            label_ids = np.argmax(logits, axis=2)
-
-                            entities = []
-                            for label_id, offset in zip(label_ids[0], data['offset_mapping']):
-                                curr_id = idx2tag[label_id]
-                                curr_start = offset[0]
-                                curr_end = offset[1]
-                                if curr_id != 'O':
-                                    if len(entities) > 0 and entities[-1]['entity'] == curr_id and curr_start - entities[-1]['end'] in [0, 1]:
-                                        entities[-1]['end'] = curr_end
-                                    else:
-                                        entities.append(
-                                            {'entity': curr_id, 'start': curr_start, 'end': curr_end})
-                            for ent in entities:
-                                ent['text'] = text[ent['start']:ent['end']]
-                            return entities
-
                     dir_list = os.listdir(app.config['EXTRACTED'])
                     for file_name in dir_list:
                         source = "C:\\WindowServer\\Flask-app\\v.1.0\\DeepBlue\\flask\\static\\extracted\\" + file_name
@@ -250,6 +194,61 @@ def upload():
                     cur.execute(
                         "INSERT INTO parse( extracted_text, cleaned_text,state, emails, linkedin_link, github_link,extra_link,phonenumber) VALUES (%s, %s, %s, %s, %s, %s, %s, %s )",
                         (text1, ftext, pincode, mailid, linkdedln, github, others, phone_number))
+                    def process_resume2(text, tokenizer, max_len):
+                        tok = tokenizer.encode_plus(
+                            text, max_length=max_len, return_offsets_mapping=True)
+
+                        curr_sent = dict()
+
+                        padding_length = max_len - len(tok['input_ids'])
+
+                        curr_sent['input_ids'] = tok['input_ids'] + \
+                            ([0] * padding_length)
+                        curr_sent['token_type_ids'] = tok['token_type_ids'] + \
+                            ([0] * padding_length)
+                        curr_sent['attention_mask'] = tok['attention_mask'] + \
+                            ([0] * padding_length)
+
+                        final_data = {
+                            'input_ids': torch.tensor(curr_sent['input_ids'], dtype=torch.long),
+                            'token_type_ids': torch.tensor(curr_sent['token_type_ids'], dtype=torch.long),
+                            'attention_mask': torch.tensor(curr_sent['attention_mask'], dtype=torch.long),
+                            'offset_mapping': tok['offset_mapping']
+                        }
+
+                    def predict(model, tokenizer, idx2tag, tag2idx, device, text):
+                        model.eval()
+                        data = process_resume2(text, tokenizer, MAX_LEN)
+                        input_ids, input_mask = data['input_ids'].unsqueeze(
+                            0), data['attention_mask'].unsqueeze(0)
+                        labels = torch.tensor([1] * input_ids.size(0),
+                                                dtype=torch.long).unsqueeze(0)
+                        with torch.no_grad():
+                            outputs = model(
+                                input_ids,
+                                token_type_ids=None,
+                                attention_mask=input_mask,
+                                labels=labels,
+                            )
+                            tmp_eval_loss, logits = outputs[:2]
+
+                        logits = logits.cpu().detach().numpy()
+                        label_ids = np.argmax(logits, axis=2)
+
+                        entities = []
+                        for label_id, offset in zip(label_ids[0], data['offset_mapping']):
+                            curr_id = idx2tag[label_id]
+                            curr_start = offset[0]
+                            curr_end = offset[1]
+                            if curr_id != 'O':
+                                if len(entities) > 0 and entities[-1]['entity'] == curr_id and curr_start - entities[-1]['end'] in [0, 1]:
+                                    entities[-1]['end'] = curr_end
+                                else:
+                                    entities.append(
+                                        {'entity': curr_id, 'start': curr_start, 'end': curr_end})
+                        for ent in entities:
+                            ent['text'] = text[ent['start']:ent['end']]
+                        return entities
                     print("------MODELS--------")
                     entities1 = predict(
                         MODEL, TOKENIZER, idx2tag, tag2idx, DEVICE, text)
